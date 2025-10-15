7a7dbed8d2268aa819a91c2dd3cef189e6acf11c
iridero <jcgarza_mx@hotmail.com>
2025-10-14 18:07:40 -0600

    CalcularValor

--- GatoMM/Nodo.cs
+++ GatoMM/Nodo.cs
@@ -12,10 +12,12 @@ namespace GatoMM
 
         public char[] Estado { get; private set; } = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
 
+        public List<Nodo> Hijos { get; set; } = null;
+
         private int[,] lineas
             = new int[,]
             {
-                {0,1,2},
+                {0,1,2 },
                 {3,4,5 },
                 {6,7,8 },
                 {0,3,6 },
@@ -24,6 +26,36 @@ namespace GatoMM
                 {0,4,8 },
                 {2,4,6 }
             };
+
+        public void CalcularValor ()
+        {
+            Valor = 0;
+            if (Gana('X'))
+            {
+                Valor = int.MaxValue;
+            }
+            else if (Gana('O'))
+            {
+                Valor = int.MinValue;
+            }
+            else {
+                for (int i = 0; i < 8; i++)
+                {
+                    if (Estado[lineas[i, 0]] != 'X'
+                        && Estado[lineas[i, 1]] != 'X'
+                        && Estado[lineas[i, 2]] != 'X')
+                    {
+                        Valor--;
+                    }
+                    if (Estado[lineas[i, 0]] != 'O'
+                        && Estado[lineas[i, 1]] != 'O'
+                        && Estado[lineas[i, 2]] != 'O')
+                    {
+                        Valor++;
+                    }
+                }
+            }
+        }
 
         private bool Gana(char xo)
         {
